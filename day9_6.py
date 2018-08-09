eeaca208b88f4591e89fb3b840e68eb3a84deb89
cpti372 <cpti372@naver.com>
2018-08-10 01:18:44 +0900

    naver

---
+++ day9_6.py
@@ -0,0 +1,26 @@
+'''
+네이버 실시간 검색 순위 출력하기 
+'''
+
+# 필요한 모듈 불러오기  
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen('http://naver.com') 
+bs= BeautifulSoup(html.read(),"html.parser")
+# 전체 페이지 소스 확인하기 
+# print(bs)
+# 순위 소스 출력 
+print(bs.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k'))
+rank_list = bs.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k')
+for i in rank_list:
+    print(i.string)    
+# 1 ~ 5위까지 출력하기 
+print('-'*5)
+for i in range(0,6):
+    print('{}위 {}'.format(i+1,rank_list[i].string)) 
+
+'''
+네이버 영화 랭킹 
+1~3위까지 출력하기 
+https://movie.naver.com/movie/sdb/rank/rmovie.nhn
+'''    
